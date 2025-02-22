9a0e76a7cbcd7b8070ba6866621b6552e36ca638
alexcory <alexcory@iastate.edu>
2025-02-22 01:25:48 -0600

    Refactor to use shinydashboard with complete UI redesign

--- frequentflyers/app.R
+++ frequentflyers/app.R
@@ -1,70 +1,85 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-library(ggplot2)
-library(dplyr)
-library(leaflet)
-library(lubridate)
-library(rsconnect)
 library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(dplyr)
+library(lubridate)
+
 source("filter_year.R")
 source("compute_flight_counts.R")
 
+ui <- dashboardPage(
+  dashboardHeader(title = "Frequent Flyers",
+                  tags$li(
+                    a(
+                      href = "https://github.com/alexcory1/FrequentFlyers",
+                      target = "_blank",
+                      icon("github"),
+                      style = "font-size: 30px; padding-top: 10px; padding-right: 10px;"
+                    ),
+                    class = "dropdown"
+                  )
+                  ),
 
-ui <- fluidPage(
-  tags$style(HTML("
-    body {
-      background-color: #AAD3DF;
-    }
-  ")),
-  div(style = "background-color: #003249; padding: 10px; color: white; text-align: center;",
-      h1("Frequent Flyers", style = "margin: 0;"),
-      h2("US Flight Map")
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("Plots", tabName = "plots", icon = icon("plane")),
+      menuItem("About", tabName = "about", icon = icon("info-circle"))
+    )
   ),
 
-  sliderInput("flight_year",
-              label = "Select Year Range",
-              min = 2013,
-              max = 2024,
-              value = c(2023, 2024),
-              step = 1,
-              sep = ""),
-
-  textInput("airport_id",
-            label = "Enter Airport Code: "),
-
-
-  leafletOutput("map", height="600px"),
-
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "map",
+              fluidRow(
+                box(width = 3,
+                    sliderInput("flight_year", "Select Year Range", min = 2013, max = 2024, value = c(2023, 2024), step = 1, sep = ""),
+                    textInput("airport_id", "Enter Airport Code:")
+                ),
+                box(width = 9, leafletOutput("map", height = "600px"))
+              )
+      ),
+      tabItem(tabName = "home",
+              div(tabName = 'header',
+                  style = "display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 20vh;
+                  font-size: 40px
+                  font-weight: bold
+                  ",
+                  h2("Welcome to Frequent Flyers"),
+              ),
+              div(tabName = "citation",
+                  style = "display: flex; justify-content: center; align-items: center; height: 0vh; font-size: 24px",
+                  p(HTML("Created By: <a href='https://www.linkedin.com/in/amcory' target='_blank'>Alex Cory</a>,
+                         <a href='https://www.linkedin.com/in/jillian-egland-3302a721a/' target='_blank'>Jillian Egland</a>,
+                         <a href='https://www.linkedin.com/in/xuan-wen-loo/' target='_blank'>Michelle Loo</a>,
+                         <a href='https://www.linkedin.com/in/snehal-arla-1a1346261/' target='_blank'>Snehal Arla</a>."))
+              ),
+            ),
+      tabItem(tabName = "plots", h2("Flight Data Summary")),
+      tabItem(tabName = "about", h2("About This Application"))
+    )
+  )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-
-  flight_data <- reactive({
-    filter_year(input$flight_year)
-  })
+  flight_data <- reactive({ filter_year(input$flight_year) })
 
   filtered_data <- reactive({
     if (input$airport_id == "") {
-      flight_data()  #Defaults to displaying all airports when no text in box
+      flight_data()
     } else {
       flight_data() %>%
         filter(airport_1 == toupper(input$airport_id) | airport_2 == toupper(input$airport_id))
     }
   })
 
-
   output$map <- renderLeaflet({
-    flight_map_data <- compute_flight_counts(filtered_data())
-
-    flight_map_data <- flight_map_data %>%
+    flight_map_data <- compute_flight_counts(filtered_data()) %>%
       mutate(total_flight_count = start_flight_count + end_flight_count)
 
     unique_airports <- flight_map_data %>%
@@ -80,21 +95,10 @@ server <- function(input, output) {
 
     leaflet() %>%
       addTiles() %>%
-      addPolylines(data = flight_map_data,
-                   lng = ~c(start_long, end_long),
-                   lat = ~c(start_lat, end_lat),
-                   color = "blue", weight = 1, opacity = 0.2) %>%
-      addCircleMarkers(data = unique_airports,
-                       lng = ~long, lat = ~lat,
-                       color = "#355834",
-                       fillColor = "#355834",
-                       fillOpacity = 1,
-                       opacity = 1,
-                       label = ~airport,
-                       radius = ~log(total_flight_count)/1.5) %>%
-      addLegend("bottomright", colors = c("#355834", "blue"),
-                labels = c("Airport", "Flight Path"), title = "Legend")
-
+      addPolylines(data = flight_map_data, lng = ~c(start_long, end_long), lat = ~c(start_lat, end_lat), color = "blue", weight = 1, opacity = 0.2) %>%
+      addCircleMarkers(data = unique_airports, lng = ~long, lat = ~lat, color = "#355834", fillColor = "#355834", fillOpacity = 1, opacity = 1, label = ~airport, radius = ~log(total_flight_count)/1.5) %>%
+      addLegend("bottomright", colors = c("#355834", "blue"), labels = c("Airport", "Flight Path"), title = "Legend")
   })
 }
+
 shinyApp(ui = ui, server = server)
